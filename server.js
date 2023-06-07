6cd385b3e0e34471e29591de6e32a501eda812f1
Je Yun <alwaysblue21@gmail.com>
2023-06-07 15:13:32 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     console.log(req.params.tipPercentage)
     res.send(req.params.tipPercentage)
 })
+/////////////////////////////////////
+///////Magic 8 Ball/////////////////
+
+app.get("/magic/:id", (req, res) => {
+    const arr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const random = Math.floor(Math.random() * arr.length)
+    res.send(`<h1>${arr[random]}</h1>`)
+})
 
 
 /////////////////////////////////
