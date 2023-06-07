120c21377c90ea7771dff0ce0f7c41b871a25f41
Je Yun <alwaysblue21@gmail.com>
2023-06-07 14:52:00 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.get("/greeting/:name", (req, res) => {
    console.log(req.params)
    res.send(`<h1>Wow! Hello there, ${req.params.name}</h1>`)
 })
+//////////////////////////////////////////////////////
+//////////tip calculator/////////////////////////////
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    console.log(req.params.tipPercentage)
+    res.send(req.params.tipPercentage)
+})
 
 
 /////////////////////////////////
